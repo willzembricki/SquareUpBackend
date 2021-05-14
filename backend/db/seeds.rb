6c1646844eacaf4197df83372fdb06f2a01701f5
iaconom821 <iaconom821@gmail.com>
2021-05-14 11:03:48 -0400

    update seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,10 +2,21 @@ Person.destroy_all
 Round.destroy_all
 PersonRound.destroy_all 
 
-4.times do 
-  Person.create(name: Faker::Name.name,balance: 0, total: 0  )
-end
 
+Person.create(name: 'Adam Schwartz',balance: 0, total: 0  )
+Person.create(name: 'Danny Costelloe',balance: 0, total: 0  )
+Person.create(name: 'Erin Sellers',balance: 0, total: 0  )
+Person.create(name: 'Gedler Jean Pierre',balance: 0, total: 0  )
+Person.create(name: 'Isabella Wiley',balance: 0, total: 0  )
+Person.create(name: 'Jon Brundage Jr',balance: 0, total: 0  )
+Person.create(name: 'Josh Zimmerman',balance: 0, total: 0  )
+Person.create(name: 'Michael Corrieri',balance: 0, total: 0  )
+Person.create(name: 'Michael Iacono',balance: 0, total: 0  )
+Person.create(name: 'Paul Graham Jr',balance: 0, total: 0  )
+Person.create(name: 'Peter Crawford',balance: 0, total: 0  )
+Person.create(name: 'Tammy Wong',balance: 0, total: 0  )
+Person.create(name: 'William Zembricki',balance: 0, total: 0  )
+Person.create(name: 'Zach Kaigler',balance: 0, total: 0  )
 
 # PersonRound.create(round_id:1,person_id:1 )
 # PersonRound.create(round_id:1,person_id:2 )
